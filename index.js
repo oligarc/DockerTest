af983265c2b176bb92603f0581ee2537aafeac19
oligarc <oliver25399@gmail.com>
2025-07-03 12:41:04 +0200

    trying

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'; //Framework Nodejs
+import mongoose from 'mongoose'; //Librería para conectar mongoDB
+
+const personaSchema = new mongoose.Schema({ //Defines un esquema Moongose para guardar en MongoDb
+    name: String,
+    age: Number,
+    email: String
+});
+
+const Persona = mongoose.model('Persona', personaSchema); //Y aquí creas el modelo basado en ese esquema
+const app = express();
+
+mongoose.connect('mongodb://oliver:password@localhost:27017/mi_base_de_datos?authSource=admin')
+
+app.get('/',async(_req,res) => {
+    console.log("Listando...");
+    const personas = await Persona.find();
+    return res.send(personas);
+})
+
+app.get('/crear',async(_req,res) => {
+    console.log('Creando nueva persona...')
+    await Persona.create({name:"Pepe",email:"pepe4@gmail.com",age:26})
+    return res.send('todo ok Jose Luis')
+})
+
+app.listen(3000,() => console.log('Escuchando...'))
